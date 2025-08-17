56eca0b60b03416410ea25abfe8670584e535afd
Anna Fritz <anna_eida@gmx.de>
2025-08-18 00:16:16 +0200

    feat(view): add VideoListView

--- content/api/views.py
+++ content/api/views.py
@@ -1,5 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from ..models import Video
+from .serializers import VideoSerializer
 
 
-class VideoViewSet(viewsets.ModelViewSet):
-    pass
+class VideoListView(generics.ListAPIView):
+    """List all completed videos"""
+    queryset = Video.objects.filter(processing_status='completed')
+    serializer_class = VideoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
+
