b32cbe3d078facf3a2a5aa3ca42746303317fd85
Anna Fritz <anna_eida@gmx.de>
2025-08-22 02:19:39 +0200

    chore(auth): update ACCESS_TOKEN_LIFETIME

--- core/settings.py
+++ core/settings.py
@@ -191,7 +191,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=21),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
