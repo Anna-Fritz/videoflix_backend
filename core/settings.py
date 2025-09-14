73a8a77040373afcd4a2fe2e6b886ef3354595db
Anna Fritz <anna_eida@gmx.de>
2025-09-14 23:45:06 +0200

    fix(auth): update access token lifetime

--- core/settings.py
+++ core/settings.py
@@ -198,7 +198,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=21),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=19),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
